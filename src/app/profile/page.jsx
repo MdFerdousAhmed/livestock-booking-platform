9109a7b0b2990dd039e69ee4ab7d063bfde18f64
MD FERDOUS AHMED <148644652+MdFerdousAhmed@users.noreply.github.com>
2026-05-01 10:51:37 +0600

    add profile

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,28 @@
+"use client"
+import { UpdateUserModal } from '@/components/UpdateUserModal';
+import { authClient } from '@/lib/auth-client';
+import { Avatar, Card } from '@heroui/react';
+import React from 'react';
+
+const ProfilePage = () => {
+  const userData = authClient.useSession();
+  const user = userData.data?.user;
+  return (
+    <div>
+      <Card className='max-w-96 mx-auto flex flex-col items-center border mt-5'>
+        <Avatar className="w-20 h-20">
+          <Avatar.Image alt="John Doe" src={user?.image}
+            referrerPolicy="no-referrer"
+          />
+          <Avatar.Fallback>F</Avatar.Fallback>
+        </Avatar>
+        <h2 className='text-xl font-bold'>{user?.name}</h2>
+        <p className='text-gray-500'>{user?.email}</p>
+
+        <UpdateUserModal></UpdateUserModal>
+      </Card>
+    </div>
+  );
+};
+
+export default ProfilePage;
