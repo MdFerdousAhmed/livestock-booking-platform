9754c85b97eb8b8ecd5b8c7c48365f2b71ce2c99
MD FERDOUS AHMED <148644652+MdFerdousAhmed@users.noreply.github.com>
2026-05-02 12:34:18 +0600

    added server link

--- src/app/all-animals/[id]/page.jsx
+++ src/app/all-animals/[id]/page.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 
 export const generateMetadata = async ({ params }) => {
   const { id } = await params;
-  const res = await fetch("http://localhost:3000/data.json");
+  const res = await fetch("https://livestock-booking-platform.vercel.app/data.json");
   const animals = await res.json();
   const animal = animals.find(a => a.id == id);
   return {
